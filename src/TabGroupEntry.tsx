d229b4f1815eacba31b97c670f8d9e828d7aac5b
Arjun Butani <abutani@student.ethz.ch>
2024-02-04 18:02:39 +0100

    Added some styling and tabs are clickable

--- src/TabGroupEntry.tsx
+++ src/TabGroupEntry.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDrag, useDrop } from 'react-dnd';
 import {ItemTypes} from "./types";
+import {FaAngleRight, FaAngleDown} from "react-icons/fa"
+import "./TabGroupEntry.css"
 
 
-const GroupHeader = (props: {groupName: string, onDrop: (TabId: browser.tabs.Tab, groupName: string ) => void}) => {
+const GroupHeader = (props: {groupName: string, isExpanded: boolean, setIsExpanded: React.Dispatch<React.SetStateAction<boolean>> , onDrop: (TabId: browser.tabs.Tab, groupName: string ) => void}) => {
     const [{canDrop, isOver}, drop] = useDrop(() => ({
         accept: ItemTypes.tab,
         drop: (Tab) => (props.onDrop(((Tab as {tab: browser.tabs.Tab}).tab as browser.tabs.Tab), props.groupName)),
@@ -13,9 +15,12 @@ const GroupHeader = (props: {groupName: string, onDrop: (TabId: browser.tabs.Tab
         })
     }));
     return (
-    <h1 ref={drop} style={{backgroundColor: (isOver && canDrop) ? 'grey' : 'red' }}>
-        {props.groupName}
-    </h1>
+        <div ref={drop} className='group-header' onClick={() => props.setIsExpanded(!props.isExpanded)} style={{ backgroundColor: (isOver && canDrop) ? 'grey' : 'red' }}>
+            {props.isExpanded ? <FaAngleDown/> : <FaAngleRight/>}
+            <h1  >
+                {props.groupName}
+            </h1>
+        </div>
     );
 }
 
@@ -28,8 +33,12 @@ const DraggableTabEntry = (props: {tab: browser.tabs.Tab}) => {
         }),
     }), [props.tab]);
 
+    const openTab = () => {
+        browser.tabs.update(props.tab.id!, {active: true});
+    } 
+
     return (
-        <div ref={drag} style={{ opacity: isDragging ? 0.5 : 1, backgroundColor: isDragging ? "grey": "white"}}>
+        <div ref={drag} style={{ opacity: isDragging ? 0.5 : 1, backgroundColor: isDragging ? "grey": "white", cursor: 'pointer'}} onClick={openTab}>
             {props.tab.title} {props.tab.id}
         </div>
     )
@@ -53,11 +62,12 @@ const TabList = (props: {tabs: browser.tabs.Tab[]}) => {
 
 const TabGroupEntry = (props: {groupName: string, tabs: browser.tabs.Tab[], dropHandler: (Tab: browser.tabs.Tab, groupName: string ) => void}) => {
     console.log("in tab group entry")
+    const [isExpanded, setIsExpanded] = useState(false);
 
     return (
         <>
-            <GroupHeader groupName={props.groupName} onDrop={props.dropHandler} />
-            <TabList tabs={props.tabs} />
+            <GroupHeader groupName={props.groupName} onDrop={props.dropHandler} isExpanded={isExpanded} setIsExpanded={setIsExpanded} />
+            {isExpanded && <TabList tabs={props.tabs} />}
         </>
     );
 }
