150c9d1c78eb79817f0f7a2467a5a2565843e9e4
AKButani <butani.arjun@gmail.com>
2024-04-20 00:20:21 +0200

    Changed Confirm Dialog to mui and removed unused packages

--- src/popup/RemoveGroup/RemoveGroup.tsx
+++ src/popup/RemoveGroup/RemoveGroup.tsx
@@ -1,12 +1,13 @@
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {ConfirmDialog} from 'primereact/confirmdialog';
+
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
 import "./RemoveGroup.css"
-import 'primereact/resources/themes/saga-blue/theme.css';
-import 'primereact/resources/primereact.min.css';
-/* import 'primeicons/primeicons.css'; */
 import React, { useContext, useState } from "react";
 import { UpdateGroupsContext } from "../GroupList";
+import { Button, DialogContentText } from "@mui/material";
 
 export const RemoveGroup = (props: {tabs: browser.tabs.Tab[], groupName: string}) => {
     /* const buttonRef = useRef(null); */
@@ -58,42 +59,21 @@ export const RemoveGroup = (props: {tabs: browser.tabs.Tab[], groupName: string}
     
     return (
         <>
-            <ConfirmDialog 
-                visible={dialogVisible}
-                accept={accept}
-                reject={() => setDialogVisible(false)}
-                onHide={() => setDialogVisible(false)}
-                message="Are you sure you want to proceed?"
-                showHeader={false}
-                dismissableMask={true}
-                /* contentStyle={{
-                    display: "flex",
-                    backgroundColor:"red",
-                    alignSelf: "center",
-                    textAlign: "left",
-                    alignItems: "center",
-                    gap: "10px",
-                }}
-                style={{
-                    gap: "10px",
-                }} */
+            <Dialog 
+                open={dialogVisible}
+                onClose={() => setDialogVisible(false)}
+            >
+                <DialogContent>
+                    <DialogContentText>
+                        Are you sure you want to close all tabs in the group?
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => setDialogVisible(false)}> No </Button>
+                    <Button onClick={accept} autoFocus> Yes </Button>
+                </DialogActions>
+            </Dialog>
 
-                pt = {{
-                    message: {
-                        style: {
-                            textAlign: "left",
-                        }
-                    }
-                }}
-                /* header="Confirmation" */
-                /* style={{ 
-                    width: '50vw', 
-                    height: '50vh',
-                    maxWidth: '80vw', // Set a maximum width
-                    maxHeight: '80vh', // Set a maximum height
-                    overflow: 'auto' // Enable scrolling if content overflows 
-                }}  */
-            />
             {/* <div className="button-remove-group-wrapper" style={{display: "flex", justifyContent: "center", backgroundColor: "red"}}> */}
             <FontAwesomeIcon  icon={faXmark} className='button-remove-group button' onClick={() => setDialogVisible(true)}/>
            {/*  </div> */}
